5d20e1aa1159de300e036f96c144c9de65c9fb96
Paul Ciarlo <paul.ciarlo@gmail.com>
2019-09-12 02:10:04 -0400

    add xcode project manually, updating/fixing assumptions based on newly published MS doc
    yeah a lot of that logging code should just be piped straight to /dev/null

--- libresurrExion/src/exfat_structs.hpp
+++ libresurrExion/src/exfat_structs.hpp
@@ -56,51 +56,51 @@ template <size_t SectorSize>
 struct sector_t
 {
     uint8_t data[SectorSize];
-};
+} __attribute__((packed));
 static_assert(sizeof(sector_t<512>) == 512);
 
 template <size_t SectorSize, size_t SectorsPerCluster>
 struct cluster_t
 {
     sector_t<SectorSize> sectors[SectorsPerCluster];
-};
+} __attribute__((packed));
 static_assert(sizeof(cluster_t<512, 512>) == 512*512);
 
 enum VolumeFlags {
+    NO_FLAGS            = 0,
     SECOND_FAT_ACTIVE   = 1<<0,
-    VOLUME_DIRTY        = 1<<1,
-    MEDIA_FAILURE       = 1<<2,
-    CLEAR_TO_ZERO       = 1<<3
+    VOLUME_DIRTY        = 1<<1, // probably in an inconsistent state
+    MEDIA_FAILURE       = 1<<2, // failed read/write and exhausted retry algorithms
+    CLEAR_TO_ZERO       = 1<<3  // clear this bit to zero on mount
+    // rest are reserved
 };
 
 template <size_t SectorSize>
-struct volume_boot_record_t {
-    uint8_t  jump_boot[3]               = {0x90, 0x76, 0xEB};   // 0xEB7690 little-endian
-    uint8_t  fs_name[8]                 = {'E', 'X', 'F', 'A', 'T', '\x20', '\x20', '\x20'};
-    uint8_t  zero[53]                   = {0};
-    uint64_t partition_offset_sectors;          // Sector address of partition
-    // (TODO is this the partition or the disk length? I think partition)
-    uint64_t volume_length_sectors;             // Size of total volume in sectors
-    uint32_t fat_offset_sectors;                // Sector address of first FAT (24?)
-    uint32_t fat_length_sectors;                // Size of FAT in sectors
-    uint32_t cluster_heap_offset_sectors;       // Sector offset of cluster heap
-    uint32_t cluster_count;                     // Number of clusters in cluster heap
-    uint32_t root_directory_cluster;            // Cluster address of root directory
-    uint32_t volume_serial_number;
-    uint16_t fs_revision                = 0x0100;
-    uint16_t volume_flags;                      // Combination of fs_volume_flags_t
-    uint8_t  bytes_per_sector;                  // Power of two
-    uint8_t  sectors_per_cluster;               // Power of two
-    // Cluster size must be in range 512-4096 so bytes_per_sector + sectors_per_cluster <= 25
-    uint8_t  num_fats                   = 1;    // 1 or 2. 2 onlyfor TexFAT (not supported)
-    uint8_t  drive_select               = 0x80; // Extended INT 13h drive number
-    uint8_t  percent_used;                      // Percentage of heap in use
+struct boot_sector_t {
+    uint8_t  jump_boot[3]              = {0x90, 0x76, 0xEB};   // 0xEB7690 little-endian
+    uint8_t  fs_name[8]                = {'E', 'X', 'F', 'A', 'T', '\x20', '\x20', '\x20'};
+    uint8_t  zero[53]                  = {0};
+    uint64_t partition_offset_sectors  = 0;        // Sector address of partition on media (0 to ignore)
+    uint64_t volume_length_sectors;                // Size of ExFAT volume in sectors
+    uint32_t fat_offset_sectors;                   // 24 <= Sector offset of FAT <= ClusterHeapOffset - FatLength
+    uint32_t fat_length_sectors;                   // Size of (each)FAT in sectors
+    uint32_t cluster_heap_offset_sectors;          // FatOffset + FatLength <= Offset of Cluster Heap
+    uint32_t cluster_count;                        // Number of clusters in cluster heap
+    uint32_t root_directory_cluster;               // 2 <= Cluster address of root directory
+    uint32_t volume_serial_number      = 0xdead;   // All values are valid!
+    uint16_t fs_revision               = 0x0100;
+    uint16_t volume_flags              = NO_FLAGS; // Combination of fs_volume_flags_t
+    uint8_t  log2_bytes_per_sector;                // eg, log2(512) == 9, 512<= bytes_per_sector <= 4096 so range [9,12]
+    uint8_t  log2_sectors_per_cluster;             // at least 0, at most 25-log2_bytes_per_sector (32MB max cluster size)
+    uint8_t  num_fats                   = 1;       // 1 or 2. 2 onlyfor TexFAT (not supported)
+    uint8_t  drive_select               = 0x80;    // Extended INT 13h drive number
+    uint8_t  percent_used               = 0xFF;    // [0,100] Percentage of heap in use or 0xFF if not available
     uint8_t  reserved[7]                = {0};
-    uint8_t  boot_code[390]             = {0};
+    uint8_t  boot_code[390]             = {0xF4};  // x86 HLT instruction
     uint16_t boot_signature             = 0xAA55;
-	//uint8_t  padding[SectorSize - 512];        // Padded out to sector size
+	uint8_t  padding[SectorSize - 512];            // Padded out to sector size
 } __attribute__((packed));
-static_assert(sizeof(volume_boot_record_t<512>) == 512);
+static_assert(sizeof(boot_sector_t<512>) == 512);
 
 // for a 512-byte sector. should be same size as a sector
 template <size_t SectorSize>
@@ -113,29 +113,26 @@ static_assert(sizeof(extended_boot_structure_t<512>) == 512);
 template <size_t SectorSize>
 struct main_extended_boot_region_t {
     extended_boot_structure_t<SectorSize> ebs[8];
-};
+} __attribute__((packed));
 static_assert(sizeof(main_extended_boot_region_t<512>) == 8*512);
+
+struct oem_parameters_null_entry_t {
+    uint8_t guid[16]     = {0};
+    uint8_t reserved[32] = {0};
+} __attribute__((packed));
+static_assert(sizeof(struct oem_parameters_null_entry_t) == 48);
 
 // First 16 bytes of each field is a GUID and remaining 32 bytes are the parameters (undefined)
 template <size_t SectorSize>
 struct oem_parameters_t {
-    uint8_t parameters0[48]             = {0};
-    uint8_t parameters1[48]             = {0};
-    uint8_t parameters2[48]             = {0};
-    uint8_t parameters3[48]             = {0};
-    uint8_t parameters4[48]             = {0};
-    uint8_t parameters5[48]             = {0};
-    uint8_t parameters6[48]             = {0};
-    uint8_t parameters7[48]             = {0};
-    uint8_t parameters8[48]             = {0};
-    uint8_t parameters9[48]             = {0};
-    uint8_t reserved[SectorSize - 48*10]= {0}; // 32-3616 bytes padded out to sector size
-};
+    struct oem_parameters_null_entry_t null_entries[10];
+    uint8_t reserved[SectorSize - 480]  = {0}; // 32-3616 bytes padded out to sector size
+} __attribute__((packed));
 static_assert(sizeof(oem_parameters_t<512>) == 512);
 
 // one example of a parameter that would go in an OEM parameter but it's not used
-struct flash_parameters {
-    uint8_t  OemParameterType[8];
+struct flash_parameters_t {
+    uint8_t  OemParameterType[16];
     uint32_t EraseBlockSize;
     uint32_t PageSize;
     uint32_t NumberOfSpareBlocks;
@@ -145,6 +142,7 @@ struct flash_parameters {
     uint32_t tWriteCycle;
     uint8_t  Reserved[4];
 } __attribute__((packed));
+static_assert(sizeof(struct flash_parameters_t) == 48);
 
 struct timestamp {
     uint8_t double_seconds[5];
@@ -198,11 +196,12 @@ enum VolumeGuidFlags {
 struct raw_entry_t {
     uint8_t type;
     uint8_t data[31];
-};
+} __attribute__((packed));
+static_assert(sizeof(struct raw_entry_t) == 32);
 
 struct file_directory_entry_t {
     uint8_t  type               = FILE_DIR_ENTRY;   // FILE_DIR_ENTRY = 0x85
-    uint8_t  continuations;
+    uint8_t  continuations; // between 2 and 18
     uint16_t checksum;
     uint16_t attributes;
     uint8_t  reserved0[2]       = {0};
@@ -216,7 +215,27 @@ struct file_directory_entry_t {
     uint8_t  modified_time_cs;
     uint8_t  accessed_time_cs;
     uint8_t  reserved1[9]       = {0};
-};
+
+    bool isValid() {
+        if (type != FILE_DIR_ENTRY) return false;
+        if (continuations < 2 || continuations > 18) return false;
+        return true;
+    }
+
+    uint16_t calc_set_checksum() {
+         int file_info_size = (continuations + 1) * 32;
+         uint8_t *bufp = (uint8_t*)this;
+         uint16_t chksum = 0;
+
+         for (int i = 0; i < file_info_size; ++i) {
+             if (i != 2 && i != 3) {
+                chksum = (((chksum << 15) & 0x8000) | (chksum >> 1)) + (uint16_t)bufp[i];
+            }
+        }
+        return chksum;
+    }
+} __attribute__((packed));
+static_assert(sizeof(struct file_directory_entry_t) == 32);
 
 struct primary_directory_entry_t {
     uint8_t  type;              // one of fs_directory_entry_t
@@ -224,17 +243,19 @@ struct primary_directory_entry_t {
     uint16_t set_checksum;      // checksum of directory entries in this set, excluding this field
     uint16_t primary_flags;     // combination of fs_file_flags_t
     uint8_t  reserved[14] = {0};
-    uint32_t first_cluster;
+    uint32_t first_cluster;     /* 0 = does not exist, otherwise, in range [2,ClusterCount+1] */
     uint64_t data_length;
-};
+} __attribute__((packed));
+static_assert(sizeof(struct primary_directory_entry_t) == 32);
 
 struct secondary_directory_entry_t {
     uint8_t  type;              // one of fs_directory_entry_t
     uint8_t  secondary_flags;   // combination of fs_file_flags_t
     uint8_t  reserved[18]   = {0};
-    uint32_t first_cluster;
+    uint32_t first_cluster;     /* 0 = does not exist, otherwise, in range [2,ClusterCount+1] */
     uint64_t data_length;
-};
+} __attribute__((packed));
+static_assert(sizeof(struct secondary_directory_entry_t) == 32);
 
 struct stream_extension_entry_t {
     uint8_t type            = STREAM_EXTENSION;
@@ -245,24 +266,31 @@ struct stream_extension_entry_t {
     uint16_t reserved1      = 0;
     uint64_t valid_size;
     uint32_t reserved2      = 0;
-    uint32_t first_cluster;
+    uint32_t first_cluster;     /* 0 = does not exist, otherwise, in range [2,ClusterCount+1] */
     uint64_t size;
-};
+    bool isValid() {
+        if (type != STREAM_EXTENSION) return false;
+        return true;
+    }
+} __attribute__((packed));
+static_assert(sizeof(struct stream_extension_entry_t) == 32);
 
 struct file_name_entry_t {
     static constexpr int FS_FILE_NAME_ENTRY_SIZE = 15;
     uint8_t type        = FILE_NAME;
     uint8_t reserved    = 0;
     int16_t name[FS_FILE_NAME_ENTRY_SIZE];
-};
+} __attribute__((packed));
+static_assert(sizeof(struct file_name_entry_t) == 32);
 
 struct allocation_bitmap_entry_t {
     uint8_t type            = ALLOCATION_BITMAP;
     uint8_t bitmap_flags    = 0; // 0 if first allocation bitmap, 1 if second (TexFAT only)
     uint8_t reserved[18]    = {0};
-    uint32_t first_cluster; // First data cluster number
+    uint32_t first_cluster  = 2;      /* 0 = does not exist, otherwise, in range [2,ClusterCount+1] */
     uint64_t data_length;   // Size of allocation bitmap in bytes. Ceil(ClusterCount / 8)
-};
+} __attribute__((packed));
+static_assert(sizeof(struct allocation_bitmap_entry_t) == 32);
 
 struct volume_guid_entry_t {
     constexpr volume_guid_entry_t() {
@@ -296,7 +324,8 @@ struct volume_guid_entry_t {
     uint16_t flags              = 0;           // combination of VolumeGuidFlags, must be 0
 	uint8_t  volume_guid[16]    = {0};         // must not be null
     uint8_t  reserved[10]       = {0};
-};
+} __attribute__((packed));
+static_assert(sizeof(struct volume_guid_entry_t) == 32);
 
 struct volume_label_entry_t {
     static constexpr size_t VOLUME_LABEL_MAX_LENGTH = 11;
@@ -312,24 +341,18 @@ struct volume_label_entry_t {
     uint8_t character_count                         = 0;            // characters in label
     uint16_t volume_label[VOLUME_LABEL_MAX_LENGTH]  = {0};
     uint8_t reserved[8]                             = {0};
-};
+} __attribute__((packed));
+static_assert(sizeof(struct volume_label_entry_t) == 32);
 
 struct upcase_table_entry_t {
-    void calc_checksum(const uint8_t *data, size_t bytes) {
-        checksum = 0;
-        for (size_t i = 0; i < bytes; i++) {
-			//?if (i != )
-            checksum = (checksum << 31) | (checksum >> 1) + data[i];
-        }
-    }
     uint8_t  type           = UPCASE_TABLE; // 0x82
     uint8_t  reserved0[3]   = {0};
     uint32_t checksum;
     uint8_t  reserved1[12]  = {0};
-    uint32_t first_cluster;
+    uint32_t first_cluster  = 3;      /* 0 = does not exist, otherwise, in range [2,ClusterCount+1] */
     uint64_t data_length;
-};
-
+} __attribute__((packed));
+static_assert(sizeof(struct upcase_table_entry_t) == 32);
 
 union metadata_entry_u {
     raw_entry_t raw;
@@ -342,17 +365,19 @@ union metadata_entry_u {
     volume_guid_entry_t volume_guid_entry;
     volume_label_entry_t volume_label_entry;
     upcase_table_entry_t upcase_table_entry;
-};
+} __attribute__((packed));
 static_assert(sizeof(metadata_entry_u) == 32);
 
-template <size_t SectorSize, size_t NumClusters>
+template <size_t NumClusters>
 struct allocation_bitmap_table_t
 {
-    static constexpr size_t BitmapSize = (NumClusters / 8) + ((NumClusters % 8) == 0 ? 0 : 1);
-    static constexpr size_t PaddingSize = SectorSize - (BitmapSize % SectorSize);
+    static constexpr size_t BitmapSize = NumClusters & 0x7 ? (NumClusters >> 3) + 1 : NumClusters >> 3;
+    //static constexpr size_t ClusterSize = SectorSize * SectorsPerCluster;
+    //static constexpr size_t PaddingSize = ClusterSize - BitmapSize;
 
+    // first bit in the bitmap (cluster 2) is the lowest-order byte
 	uint8_t bitmap[BitmapSize] = {0};
-	uint8_t padding[PaddingSize] = {0};
+	//uint8_t padding[PaddingSize] = {0};
 
 	constexpr allocation_bitmap_table_t() {
 		mark_all_alloc();
@@ -363,9 +388,14 @@ struct allocation_bitmap_table_t
 			bitmap[i] = 0xFF;
 		}
 	}
-};
-// idk what this is supposed to check!
-//  static_assert(sizeof(allocation_bitmap_table_t<512, >::bitmap) == 29806);
+
+    constexpr allocation_bitmap_entry_t get_directory_entry() const {
+        return allocation_bitmap_entry_t {
+            .data_length = BitmapSize
+        };
+    }
+} __attribute__((packed));
+static_assert(sizeof(struct allocation_bitmap_table_t<29806>) == 3726);
 
 template <int SectorSize, int NumEntries>
 struct upcase_table_t {
@@ -390,23 +420,42 @@ struct upcase_table_t {
         }
     }
     char16_t entries[NumEntries];
+
+    constexpr upcase_table_entry_t get_directory_entry() const {
+        const uint8_t *data = (const uint8_t*)entries;
+        size_t sz_bytes = sizeof(char16_t) * NumEntries;
+        uint32_t chksum = 0;
+
+        for (size_t i = 0; i < sz_bytes; ++i) {
+            chksum = (((chksum<<31) & 0x80000000) | (chksum >> 1)) + (uint32_t)data[i];
+        }
+
+        return upcase_table_entry_t {
+            .data_length = sz_bytes,
+            .checksum = chksum,
+        };
+    }
 } __attribute__((packed));
+static_assert(sizeof(struct file_directory_entry_t) == 32);
 
 template <size_t SectorSize>
 struct reserved_sector_t {
     uint8_t reserved[SectorSize] = {0};
 } __attribute__((packed));
+static_assert(sizeof(struct reserved_sector_t<512>) == 512);
 
 template <size_t SectorSize>
-struct checksum_sector_t {
-    uint32_t checksum[SectorSize / 4];
+struct boot_checksum_sector_t {
+    uint32_t checksum[SectorSize / 4]; // same 32-bit checksum thing repeated
 
-    void calculate_checksum(uint8_t *vbr, size_t vbr_size)
+    void fill_checksum(uint8_t *vbr, size_t vbr_size)
     {
+        assert(vbr_size == SectorSize * 11);
+
         uint32_t chksum = 0;
         for (size_t i = 0; i < vbr_size; ++i) {
             if (i != 106 && i != 107 && i != 112) {
-                chksum = ((chksum << 31) | (chksum >> 1)) + vbr[i];
+                chksum = (((chksum << 31) &0x80000000) | (chksum >> 1)) + (uint32_t)vbr[i];
             }
         }
         for (size_t i = 0; i < sizeof(checksum); ++i) {
@@ -414,15 +463,17 @@ struct checksum_sector_t {
         }
     }
 } __attribute__((packed));
+static_assert(sizeof(struct boot_checksum_sector_t<512>) == 512);
 
 template <size_t SectorSize>
 struct boot_region_t {
-    volume_boot_record_t<SectorSize>        vbr;            // Sector 0
+    boot_sector_t<SectorSize>               vbr;            // Sector 0
     main_extended_boot_region_t<SectorSize> mebs;           // Sector 1-8
     oem_parameters_t<SectorSize>            oem_params;     // Sector 9
     reserved_sector_t<SectorSize>           reserved;       // Sector 10
-    checksum_sector_t<SectorSize>           checksum;       // Sector 11
+    boot_checksum_sector_t<SectorSize>      checksum;       // Sector 11
 } __attribute__((packed));
+static_assert(sizeof(boot_region_t<512>) == 12*512);
 
 enum FatEntrySpecial {
     BAD_CLUSTER                 = 0xFFFFFFF7,
@@ -459,22 +510,6 @@ struct root_directory_t
 
 struct directory_t
 {
-    void calc_entry_set_checksum(uint8_t secondary_count) {
-        primary_entry.secondary_count = secondary_count;
-        uint16_t &chksum = primary_entry.set_checksum = 0;
-        const uint8_t *data = (const uint8_t*)&primary_entry;
-        for (int i = 0; i < sizeof(primary_directory_entry_t); ++i) {
-            if (i != 2 && i != 3) {
-                chksum = ((chksum << 31) | (chksum >> 1)) + data[i];
-            }
-        }
-        for (size_t sec_entry = 0; sec_entry < secondary_count; ++sec_entry) {
-            data = (const uint8_t*)(secondary_entries + sec_entry);
-            for (int i = 0; i < sizeof(secondary_directory_entry_t); ++i) {
-                chksum = ((chksum << 31) | (chksum >> 1)) + data[i];
-            }
-        }
-    }
     primary_directory_entry_t primary_entry;
     secondary_directory_entry_t secondary_entries[0];
 } __attribute__((packed));
@@ -506,6 +541,7 @@ struct data_region_t {
 
 template <size_t SectorSize, size_t SectorsPerCluster, size_t NumSectors>
 struct filesystem_t {
+    constexpr static size_t ClusterSize = SectorSize * SectorsPerCluster;
     constexpr static size_t ClustersInFat = (NumSectors - 0x283D8) / 512;
     boot_region_t<SectorSize>                                                  main_boot_region;
     // copy of main_boot_region
